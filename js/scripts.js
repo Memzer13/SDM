51a430e329ca840316cfb2f26334df37b3b0320c
Memzer13 <70153372+Memzer13@users.noreply.github.com>
2020-10-13 22:52:08 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,14 @@ $('.navTrigger').click(function () {
 
 });
 
-
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+        $('.nav').addClass('affix');
+        console.log("OK");
+        } else {
+        $('.nav').removeClass('affix');
+    }
+});
 
 // Open the Modal
 function openModal() {
